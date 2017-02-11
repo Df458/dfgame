6d0f83bb72334115ba706892dbbfe26760803483
Hugues Ross <hugues.ross@gmail.com>
2017-02-11 18:21:38 -0500

    Added vector implementation

--- src/math/vector.c
+++ src/math/vector.c
@@ -1,17 +1,112 @@
-#include "util.h"
+// Log category, used to filter logs
+#define LOG_CATEGORY "Math"
+
 #include "vector.h"
 
-float vec2_dot(vec2 a, vec2 b)
-{
-    return (a.x * b.x) + (a.y * b.y);
+#include "log/log.h"
+#include <math.h>
+
+// Returns the length of the vactor
+// NOTE: When comparing two vectors, vec_len_squared may be preferrable as it's
+// much faster.
+float vec2_len(vec2 v) {
+    return sqrt(vec2_len_squared(v));
+}
+float vec3_len(vec3 v) {
+    return sqrt(vec3_len_squared(v));
+}
+float vec4_len(vec4 v) {
+    return sqrt(vec4_len_squared(v));
 }
 
-float vec3_dot(vec3 a, vec3 b)
-{
-    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+// Returns the squared length of the vactor
+float vec2_len_squared(vec2 v) {
+    return pow(v.x, 2) + pow(v.y, 2);
+}
+float vec3_len_squared(vec3 v) {
+    return pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2);
+}
+float vec4_len_squared(vec4 v) {
+    return pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2) + pow(v.w, 2);
 }
 
-float vec4_dot(vec4 a, vec4 b)
-{
-    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z) + (a.w * b.w);
+// Normalizes the vector, making its length 1.
+vec2 vec2_normalize(vec2 v) {
+    float len = vec2_len(v);
+
+    if(len == 0) {
+        error("Attempting to normalize a vec2 of 0 length is undefined.");
+        return (vec2){ 0 };
+    }
+
+    return (vec2){ .x = v.x / len, .y = v.y / len };
+}
+vec3 vec3_normalize(vec3 v) {
+    float len = vec3_len(v);
+
+    if(len == 0) {
+        error("Attempting to normalize a vec3 of 0 length is undefined.");
+        return (vec3){ 0 };
+    }
+
+    return (vec3){ .x = v.x / len, .y = v.y / len, .z = v.z / len };
+}
+vec4 vec4_normalize(vec4 v) {
+    float len = vec4_len(v);
+
+    if(len == 0) {
+        error("Attempting to normalize a vec4 of 0 length is undefined.");
+        return (vec4){ 0 };
+    }
+
+    return (vec4){ .x = v.x / len, .y = v.y / len, .z = v.z / len, .w = v.w / len };
+}
+
+// Adds 2 vectors together
+vec2 vec2_add(vec2 v1, vec2 v2) {
+    return (vec2){ .x = v1.x + v2.x, .y = v1.y + v2.y };
+}
+vec3 vec3_add(vec3 v1, vec3 v2) {
+    return (vec3){ .x = v1.x + v2.x, .y = v1.y + v2.y, .z = v1.z + v2.z };
+}
+vec4 vec4_add(vec4 v1, vec4 v2) {
+    return (vec4){ .x = v1.x + v2.x, .y = v1.y + v2.y, .z = v1.z + v2.z, v1.w + v2.w };
+}
+
+// Subtracts v2 from v1
+vec2 vec2_sub(vec2 v1, vec2 v2) {
+    return (vec2) { .x = v1.x - v2.x, .y = v1.y - v2.y };
+}
+vec3 vec3_sub(vec3 v1, vec3 v2) {
+    return (vec3) { .x = v1.x - v2.x, .y = v1.y - v2.y, .z = v1.z - v2.z };
+}
+vec4 vec4_sub(vec4 v1, vec4 v2) {
+    return (vec4) { .x = v1.x - v2.x, .y = v1.y - v2.y, .z = v1.z - v2.z, .w = v1.w - v2.w };
+}
+
+// Multiplies v by the scalar x
+vec2 vec2_mul(vec2 v, float f) {
+    return (vec2) { .x = v.x * f, .y = v.y * f };
+}
+vec3 vec3_mul(vec3 v, float f) {
+    return (vec3) { .x = v.x * f, .y = v.y * f, .z = v.z * f };
+}
+vec4 vec4_mul(vec4 v, float f) {
+    return (vec4) { .x = v.x * f, .y = v.y * f, .z = v.z * f, .w = v.w * f };
+}
+
+// Returns the dot product between v1 and v2
+float vec2_dot(vec2 v1, vec2 v2) {
+    return (v1.x * v2.x) + (v1.y * v2.y);
+}
+float vec3_dot(vec3 v1, vec3 v2) {
+    return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+}
+float vec4_dot(vec4 v1, vec4 v2) {
+    return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z) + (v1.w * v2.w);
+}
+
+// Returns the cross product of v1 and v2.
+vec3 vec3_cross(vec3 v1, vec3 v2) {
+    return (vec3){ .x = (v1.y * v2.z) - (v1.z * v2.y), .y = (v1.z * v2.x) - (v1.x * v2.z), .z = (v1.x * v2.y) - (v1.y * v2.x) };
 }
--- src/math/vector.h
+++ src/math/vector.h
@@ -1,6 +1,7 @@
-#ifndef DF_VECTOR_H
-#define DF_VECTOR_H
+#ifndef DF_MATH_VECTOR
+#define DF_MATH_VECTOR
 
+// Represents a 2D vector
 typedef struct vec2 {
     union {
         struct {
@@ -10,6 +11,8 @@ typedef struct vec2 {
         float data[2];
     };
 } vec2;
+
+// Represents a 3D vector
 typedef struct vec3 {
     union {
         struct {
@@ -17,9 +20,17 @@ typedef struct vec3 {
             float y;
             float z;
         };
+        struct {
+            float r;
+            float g;
+            float b;
+        };
         float data[3];
+        vec2 xy;
     };
 } vec3;
+
+// Represents a 4D vector
 typedef struct vec4 {
     union {
         struct {
@@ -28,14 +39,95 @@ typedef struct vec4 {
             float z;
             float w;
         };
+        struct {
+            float r;
+            float g;
+            float b;
+            float a;
+        };
         float data[4];
+        vec3 xyz;
+        vec3 rgb;
+        vec2 xy;
     };
 } vec4;
 
-float vec2_dot(vec2 a, vec2 b);
+// Returns the length of the vactor.
+// NOTE: When comparing two vectors, vec_len_squared may be preferrable as it's
+// much faster.
+#define vec_len(v) _Generic(v,\
+    vec2: vec2_len(v),\
+    vec3: vec3_len(v),\
+    vec4: vec4_len(v)\
+)
+float vec2_len(vec2 v);
+float vec3_len(vec3 v);
+float vec4_len(vec4 v);
 
-float vec3_dot(vec3 a, vec3 b);
+// Returns the squared length of the vactor.
+#define vec_len_squared(v) _Generic(v,\
+    vec2: vec2_len_squared(v),\
+    vec3: vec3_len_squared(v),\
+    vec4: vec4_len_squared(v)\
+)
+float vec2_len_squared(vec2 v);
+float vec3_len_squared(vec3 v);
+float vec4_len_squared(vec4 v);
 
-float vec4_dot(vec4 a, vec4 b);
+// Normalizes the vector, making its length 1.
+#define vec_normalize(v) _Generic(v,\
+    vec2: vec2_normalize(v),\
+    vec3: vec3_normalize(v),\
+    vec4: vec4_normalize(v)\
+)
+vec2 vec2_normalize(vec2 v);
+vec3 vec3_normalize(vec3 v);
+vec4 vec4_normalize(vec4 v);
+
+// Adds 2 vectors together
+#define vec_add(v1, v2) _Generic(v1,\
+    vec2: vec2_add(v1, v2),\
+    vec3: vec3_add(v1, v2),\
+    vec4: vec4_add(v1, v2)\
+)
+vec2 vec2_add(vec2 v1, vec2 v2);
+vec3 vec3_add(vec3 v1, vec3 v2);
+vec4 vec4_add(vec4 v1, vec4 v2);
+
+// Subtracts v2 from v1
+#define vec_sub(v1, v2) _Generic(v1,\
+    vec2: vec2_sub(v1, v2),\
+    vec3: vec3_sub(v1, v2),\
+    vec4: vec4_sub(v1, v2)\
+)
+vec2 vec2_sub(vec2 v1, vec2 v2);
+vec3 vec3_sub(vec3 v1, vec3 v2);
+vec4 vec4_sub(vec4 v1, vec4 v2);
+
+// Multiplies v by the scalar x.
+#define vec_mul(v, f) _Generic(v,\
+    vec2: vec2_mul(v1, f),\
+    vec3: vec3_mul(v1, f),\
+    vec4: vec4_mul(v1, f)\
+)
+vec2 vec2_mul(vec2 v, float f);
+vec3 vec3_mul(vec3 v, float f);
+vec4 vec4_mul(vec4 v, float f);
+
+// Returns the dot product between v1 and v2.
+#define vec_dot(v1, v2) _Generic(v1,\
+    vec2: vec2_dot(v1, v2),\
+    vec3: vec3_dot(v1, v2),\
+    vec4: vec4_dot(v1, v2)\
+)
+float vec2_dot(vec2 v1, vec2 v2);
+float vec3_dot(vec3 v1, vec3 v2);
+float vec4_dot(vec4 v1, vec4 v2);
+
+// Returns the cross product of v1 and v2.
+#define vec_cross(v1, v2) _Generic(v1,\
+    vec3: vec3_cross(v1, v2),\
+)
+vec3 vec3_cross(vec3 v1, vec3 v2);
 
 #endif
